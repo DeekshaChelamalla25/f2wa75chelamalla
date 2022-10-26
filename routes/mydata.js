2f6d9a4db691380a4d2604d20abdfe0bb3448989
DeekshaChelamalla25 <112186227+DeekshaChelamalla25@users.noreply.github.com>
2022-10-26 11:48:55 -0500

    Update mydata.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+var data = require('express');
+var router = data.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Deeksha Chelamalla' });
+  res.render('mydata', { title: 'Deeksha Chelamalla' });
 });
 
 module.exports = router;
