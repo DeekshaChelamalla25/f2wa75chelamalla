d37b1b4bc859a8cb75b9e0fe1ff0a15cf5a0862d
DeekshaChelamalla25 <112186227+DeekshaChelamalla25@users.noreply.github.com>
2022-10-26 22:12:59 -0500

    updated my data

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydataRouter = require('./routes/mydata');
-
+//var computationRouter = require('./routes/computation');
 var app = express();
 
 // view engine setup
@@ -20,9 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', mydataRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/mydata', mydataRouter);
+//app.use('/computation', computationRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
